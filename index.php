2913e16ad99d0cc2c87f48614123340e632e6a3f
vxs <vxs@192.168.10.224>
2016-12-19 19:01:54 +0100

    Premier commit ( en local )

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Projet GIT ISFCE DvpWeb</h1>
+        <p>Lundi 19 decembre 2016</p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
